8f0c5f6d8af7237d577871d1f714b00a98e7a328
Merlind Pohl <merlind.pohl@student.htw-berlin.de>
2025-02-18 19:36:51 +0100

    Navigation und Route bearbeitet

--- first/src/app/nav/nav.component.ts
+++ first/src/app/nav/nav.component.ts
@@ -6,7 +6,7 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
   selector: 'app-nav',
   standalone: true,
   imports: [RouterLink, RouterLinkActive],
-  templateUrl: './nav.component.html',
+  templateUrl: './nav.component.html' ,
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
