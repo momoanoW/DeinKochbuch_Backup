ef5b1c319e636709f5bc9cde388bec970ea67422
Merlind Pohl <merlind.pohl@student.htw-berlin.de>
2025-02-18 21:54:47 +0100

    DB Anbindung

---
+++ backend/db.js
@@ -0,0 +1,20 @@
+const pg = require('pg');
+require('dotenv').config();
+
+const client = new pg.Client({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+});
+
+client.connect(err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Connection to DB ...');
+    }
+});
+
+module.exports = client;
