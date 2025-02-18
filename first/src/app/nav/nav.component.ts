38e56dc2e13539c2e2425cdb023af97e9cd6be5f
Merlind Pohl <merlind.pohl@student.htw-berlin.de>
2025-02-18 19:35:00 +0100

    Navigation und Route bearbeitet

--- first/src/app/nav/nav.component.ts
+++ first/src/app/nav/nav.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
